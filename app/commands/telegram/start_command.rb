eeddaddc1c6c84b52a8317bc8ecf9f8791fa7dc2
Anton <antokorp@gmail.com>
2017-01-21 23:15:06 +0300

    Returned bin/rails, bin/rake. Delete .rake_task~ file. Cleaned up my comments in runner.rb. Removed logs from start_comman.rb

--- app/commands/telegram/start_command.rb
+++ app/commands/telegram/start_command.rb
@@ -1,12 +1,9 @@
 module Telegram
   class StartCommand < BaseCommand
     def execute
-      log_info "Got /start message"
       reply_text "Hello #{message.from.first_name}"
-
-      log_info "Add new TelegramUser"
       
-      tmpUser = TelegramUser.new(:telegram_chat_id => message.chat.id, :first_name => message.from.first_name, :last_name => message.from.last_name, :username => message.from.username)
+      tmpUser = TelegramUser.new(:telegram_chat_id => message.chat.id, :telegram_user_id => message.from.id, :first_name => message.from.first_name, :last_name => message.from.last_name, :username => message.from.username)
       tmpUser.save
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121165413) do
+ActiveRecord::Schema.define(version: 20170121200335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170121165413) do
     t.string   "username"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "telegram_user_id"
   end
 
   create_table "users", force: :cascade do |t|

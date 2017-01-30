c3bca05e54ab56630c99d90435e355c336b8c394
Alexander Fedulin <alexander.fedulin@gmail.com>
2017-01-30 12:28:26 +0300

    Fixed send message notifications job

--- app/jobs/send_message_notifications_job.rb
+++ app/jobs/send_message_notifications_job.rb
@@ -3,12 +3,12 @@ class SendMessageNotificationsJob < ApplicationJob
 
   def perform(message)
     return if message.sent?
-    amount = 0
+    @amount = 0
     message.notifications.not_sent.find_each do |notification|
       notification.notify
-      amount += 1
+      @amount += 1
     end
   ensure
-    message.add_sent_amount(amount)
+    message.add_sent_amount(@amount) if @amount
   end
 end

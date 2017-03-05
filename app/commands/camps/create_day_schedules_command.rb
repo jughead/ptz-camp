b504da04645b42cf828df3f6733b3012c9c85971
xxxboom <superxxxboom@gmail.com>
2017-03-05 22:03:42 +0300

    Removed strange condition from create_day_schedules_command

--- app/commands/camps/create_day_schedules_command.rb
+++ app/commands/camps/create_day_schedules_command.rb
@@ -1,6 +1,6 @@
 class Camps::CreateDaySchedulesCommand
   DEFAULT_CONTENT = "08:30 – Breakfast\n" +
-                    "09:30 – 14:30 – Contest\n" +
+                    "09:30 - 14:30 – Contest\n" +
                     "14:30 – Lunch\n" +
                     "15:30 – Problem analysis\n" +
                     "19:00 – Dinner\n" +
@@ -11,17 +11,12 @@ class Camps::CreateDaySchedulesCommand
   end
 
   def execute
+    start = @camp.start_date
+    finish = @camp.finish_date
     ActiveRecord::Base.transaction do
-      start = @camp.start_date
-      finish = @camp.finish_date
-      if finish - start < 1.month
-        day = start
-        while day <= finish do
-          DaySchedule.create!(camp: @camp, date: day, content: DEFAULT_CONTENT)
-          day = day.next_day
-        end
-      else
-        # raise some error
+      while day <= finish do
+        DaySchedule.create!(camp: @camp, date: day, content: DEFAULT_CONTENT)
+        day = day.next_day
       end
     end
   end

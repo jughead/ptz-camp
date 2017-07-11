176fccf33ca557abe99c41aaa1938aadca09e909
Alexander Fedulin <alexander.fedulin@gmail.com>
2017-07-11 19:15:31 +0300

    Fixed assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( ckeditor/**/*.css ckeditor/**/*.js ckeditor/styles.js ckeditor/config.js ckeditor/contents.css )
+Rails.application.config.assets.precompile += %w( ckeditor/**/*.css ckeditor/**/*.js ckeditor/styles.js ckeditor/config.js ckeditor/contents.css ckeditor/**/*.png )
--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,2 +1,2 @@
 NonStupidDigestAssets.whitelist += [/ckeditor\/.*\.css/, /ckeditor\/.*\.js/,
-  'ckeditor/styles.js', 'ckeditor/config.js', 'ckeditor/contents.css']
+  'ckeditor/styles.js', 'ckeditor/config.js', 'ckeditor/contents.css', /ckeditor\/.*\.png/]

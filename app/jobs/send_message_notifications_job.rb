fa037d2fd2b9c89dd7e14778170cad7761be6a12
Alexander Fedulin <alexander.fedulin@gmail.com>
2018-02-05 01:32:04 +0300

    Fixed Telegram notification when the chat is deleted

--- app/jobs/send_message_notifications_job.rb
+++ app/jobs/send_message_notifications_job.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
 class SendMessageNotificationsJob < ApplicationJob
   queue_as :default
   FORBIDDEN = 403.freeze
+  BAD_REQUEST = 400.freeze
+  CHAT_NOT_FOUND = 'Bad Request: chat not found'
 
   def perform(message)
     return if message.sent?
@@ -10,10 +13,19 @@ class SendMessageNotificationsJob < ApplicationJob
         notification.notify
         @amount += 1
       rescue Telegram::Bot::Exceptions::ResponseError => e
-        raise e unless e.error_code.to_i == FORBIDDEN
+        raise e unless allowed_error?(e)
       end
     end
   ensure
     message.add_sent_amount(@amount) if @amount
+  end
+
+  private
+
+  def allowed_error?(error)
+    code = error.error_code.to_i
+    return true if code == FORBIDDEN
+    return true if code == BAD_REQUEST && error.to_s.include?(CHAT_NOT_FOUND)
+    false
   end
 end
